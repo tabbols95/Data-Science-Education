3e9d9691ea6f43afe1c122d1974a770dcddea6cb
a.shilov <tabbols95@gmail.com>
2024-11-28 19:23:08 +0300

    Регуляризация

--- Machine learning algorithms/Linear models/linear_regression.py
+++ Machine learning algorithms/Linear models/linear_regression.py
@@ -26,6 +26,17 @@ class MyLineReg:
         self._loss_value = None
         self.weights = None
 
+        # регуляризация
+        self.reg = kwargs.get("reg")
+        self.l1_coef = 0
+        self.l2_coef = 0
+
+        if self.reg == "l1" or self.reg == "elasticnet":
+            self.l1_coef = kwargs.get("l1_coef")
+
+        if self.reg == "l2" or self.reg == "elasticnet":
+            self.l2_coef = kwargs.get("l2_coef")
+
     def __repr__(self):
         return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
 
@@ -36,10 +47,10 @@ class MyLineReg:
 
         for i in range(self.n_iter):
             y_predict = (X.values @ self.weights)  # Используем матричное умножение
-            self._loss_value = ((y - y_predict) ** 2).sum() / observation_count  # mse
+            self._loss_value = ((y - y_predict) ** 2).sum() / observation_count + self.l1_coef * self.weights.sum() + self.l2_coef * (self.weights ** 2).sum()  # mse
 
-            gradient = 2 / observation_count * X.T.values @ (y_predict - y)
-            self.weights -= self.learning_rate * gradient
+            gradient = 2 / observation_count * X.T.values @ (y_predict - y) + self.l1_coef * np.sign(self.weights) + self.l2_coef * 2 * self.weights
+            self.weights -= (self.learning_rate(i+1) if callable(self.learning_rate) else self.learning_rate) * gradient
 
             if verbose and (i == 0 or i % (self.n_iter // 10) == 0):  # Вывод каждые 10%
                 print(f"{i} | loss: {self._loss_value:.2f}",
