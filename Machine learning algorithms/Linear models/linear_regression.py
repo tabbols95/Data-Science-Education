734c2cf8800bfd2c9c924461fad1d7f512aeb6e1
a.shilov <tabbols95@gmail.com>
2025-02-22 21:19:57 +0300

    Метрика оценки точности модели по-умолчанию `mse`. Оптимизация метрик расчета точности модели. Ф-ция `predict` отдает число, а не вектор.

--- Machine learning algorithms/Linear models/linear_regression.py
+++ Machine learning algorithms/Linear models/linear_regression.py
@@ -12,7 +12,7 @@ class MyLineReg:
     learning_rate: float = 0.01  # Значение по умолчанию
     """Коэффициент скорости обучения градиентного спуска"""
 
-    metric: Optional[Literal["mae", "mse", "rmse", "mape", "r2"]] = None
+    metric: Optional[Literal["mae", "mse", "rmse", "mape", "r2"]] = "mse"
     """Метрика оценки точности модели"""
 
     weights: Optional[np.ndarray] = None
@@ -52,32 +52,20 @@ class MyLineReg:
             gradient = 2 / observation_count * X.T.values @ (y_predict - y) + self.l1_coef * np.sign(self.weights) + self.l2_coef * 2 * self.weights
             self.weights -= (self.learning_rate(i+1) if callable(self.learning_rate) else self.learning_rate) * gradient
 
-            if verbose and (i == 0 or i % (self.n_iter // 10) == 0):  # Вывод каждые 10%
-                print(f"{i} | loss: {self._loss_value:.2f}",
-                      f"| {self.metric}: {self._metric_value:.2f}" if self.metric is not None else "")
-
         y_predict = (X.values @ self.weights)
 
-        if self.metric == "mae":
-            # mean absolute error
-            self._metric_value = (y - y_predict).abs().sum() / observation_count
-        elif self.metric == "rmse":
-            # root mean squared error
-            self._metric_value = np.sqrt(((y - y_predict) ** 2).sum() / observation_count)
-        elif self.metric == "mape":
-            # mean absolute percentage error
-            self._metric_value = 100 / observation_count * ((y - y_predict) / y).abs().sum()
-        elif self.metric == "r2":
-            # coefficient of determination
-            self._metric_value = 1 - (((y - y_predict) ** 2).sum() / ((y - y.mean()) ** 2).sum())
-        else:
-            # mean squared error
-            self._metric_value = ((y - y_predict) ** 2).sum() / observation_count
+        self._metric_value = {
+            "mae": (y - y_predict).abs().sum() / observation_count,
+            "mse": ((y - y_predict) ** 2).sum() / observation_count,
+            "rmse": np.sqrt(((y - y_predict) ** 2).sum() / observation_count),
+            "mape": 100 / observation_count * ((y - y_predict) / y).abs().sum(),
+            "r2": 1 - (((y - y_predict) ** 2).sum() / ((y - y.mean()) ** 2).sum())
+        }[self.metric]
 
     def predict(self, X: pd.DataFrame):
         if "x_0" not in X.columns:
             X.insert(0, "x_0", 1)
-        y_predict = (X.values @ self.weights)
+        y_predict = (X.values @ self.weights).sum()
         return y_predict
 
     def get_coef(self):
